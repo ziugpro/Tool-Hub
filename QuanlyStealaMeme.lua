28ccd74f5794527f990ea3108756b5784358e450
ziugpro <nsnssnnsnssjsjskskan@gmail.com>
2025-08-03 03:21:06 +0700

    Update QuanlyStealaMeme.lua

--- QuanlyStealaMeme.lua
+++ QuanlyStealaMeme.lua
@@ -192,12 +192,75 @@ Tab:AddButton("Left", "Dash Through Wall", function()
     end)
 end)
 Tab:AddTextLabel("Left", "Teleport")
-Tab:AddButton("Left", "TP To Base", function()
+Tab:AddButton("Left", "TP To Base 1", function()
     local player = game.Players.LocalPlayer
     local character = player and player.Character
     local root = character and character:FindFirstChild("HumanoidRootPart")
     if root then
-        root.CFrame = CFrame.new(0, 10, 0)
+        root.CFrame = CFrame.new(1732.5264892578125, 66.62960815429688, -563.0668334960938)
+    end
+end)
+
+Tab:AddButton("Left", "TP To Base 2", function()
+    local player = game.Players.LocalPlayer
+    local character = player and player.Character
+    local root = character and character:FindFirstChild("HumanoidRootPart")
+    if root then
+        root.CFrame = CFrame.new(1832.3839111328125, 66.62960815429688, -555.3934936523438)
+    end
+end)
+
+Tab:AddButton("Left", "TP To Base 3", function()
+    local player = game.Players.LocalPlayer
+    local character = player and player.Character
+    local root = character and character:FindFirstChild("HumanoidRootPart")
+    if root then
+        root.CFrame = CFrame.new(1732.280029296875, 66.62960815429688, -311.3112487792969)
+    end
+end)
+
+Tab:AddButton("Left", "TP To Base 4", function()
+    local player = game.Players.LocalPlayer
+    local character = player and player.Character
+    local root = character and character:FindFirstChild("HumanoidRootPart")
+    if root then
+        root.CFrame = CFrame.new(1833.0181884765625, 66.62960815429688, -312.1700134277344)
+    end
+end)
+
+Tab:AddButton("Left", "TP To Base 5", function()
+    local player = game.Players.LocalPlayer
+    local character = player and player.Character
+    local root = character and character:FindFirstChild("HumanoidRootPart")
+    if root then
+        root.CFrame = CFrame.new(1933.1187744140625, 66.62960815429688, -558.0361328125)
+    end
+end)
+
+Tab:AddButton("Left", "TP To Base 6", function()
+    local player = game.Players.LocalPlayer
+    local character = player and player.Character
+    local root = character and character:FindFirstChild("HumanoidRootPart")
+    if root then
+        root.CFrame = CFrame.new(1934.021240234375, -60.446285247802734, -314.5580139160156)
+    end
+end)
+
+Tab:AddButton("Left", "TP To Base 7", function()
+    local player = game.Players.LocalPlayer
+    local character = player and player.Character
+    local root = character and character:FindFirstChild("HumanoidRootPart")
+    if root then
+        root.CFrame = CFrame.new(2033.0687255859375, 75.74996185302734, -534.0536499023438)
+    end
+end)
+
+Tab:AddButton("Left", "TP To Base 8", function()
+    local player = game.Players.LocalPlayer
+    local character = player and player.Character
+    local root = character and character:FindFirstChild("HumanoidRootPart")
+    if root then
+        root.CFrame = CFrame.new(2033.421630859375, 76.94627380371094, -317.597412109375)
     end
 end)
 Tab:AddTextbox("Right", "Webhook", "", function(text)
