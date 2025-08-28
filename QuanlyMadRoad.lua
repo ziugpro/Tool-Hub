68656dfcd26dd5dc1412a3c01e9266b286717a8a
ziugpro <nsnssnnsnssjsjskskan@gmail.com>
2025-08-28 15:35:55 +0700

    Update QuanlyMadRoad.lua

--- QuanlyMadRoad.lua
+++ QuanlyMadRoad.lua
@@ -344,11 +344,66 @@ Options.PlayerESP:SetValue(false)
 
 local Main = Tabs.Main:AddSection("Teleport")
 Tabs.Main:AddButton({
-        Title = "Teleport To Win",
-        Callback = function()
-            ForceTeleport(CFrame.new(3.372170925140381, 6.251251220703125, 38088.98046875), 1)
-        end
-    })
+  Title = "Auto Win (No Stop)",
+  Callback = function()
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local PlayerGui = Player:WaitForChild("PlayerGui")
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "KaitunUI"
+ScreenGui.ResetOnSpawn = false
+ScreenGui.IgnoreGuiInset = true
+ScreenGui.DisplayOrder = 99
+ScreenGui.Parent = PlayerGui
+
+local FullBlack = Instance.new("Frame")
+FullBlack.Size = UDim2.new(1, 0, 1, 36)
+FullBlack.Position = UDim2.new(0, 0, -0.03, 0)
+FullBlack.BackgroundColor3 = Color3.fromRGB(10, 10, 10)
+FullBlack.BorderSizePixel = 0
+FullBlack.ZIndex = 19999999
+FullBlack.BackgroundTransparency = 0.4
+FullBlack.Parent = ScreenGui
+
+local Image = Instance.new("ImageLabel")
+Image.Size = UDim2.new(0, 100, 0, 100)
+Image.Position = UDim2.new(0.5, -50, 0.5, -90)
+Image.BackgroundTransparency = 1
+Image.Image = "rbxthumb://type=Asset&id=131484641795167&w=420&h=420"
+Image.ZIndex = 29999999
+Image.Parent = ScreenGui
+
+local Version = Instance.new("TextLabel")
+Version.AnchorPoint = Vector2.new(0.5, 0.5)
+Version.Position = UDim2.new(0.5, 0, 0.5, 40)
+Version.Size = UDim2.new(0, 600, 0, 50)
+Version.BackgroundTransparency = 1
+Version.Text = "Aura Hub"
+Version.Font = Enum.Font.PermanentMarker
+Version.TextSize = 55
+Version.TextColor3 = Color3.fromRGB(255, 255, 255)
+Version.ZIndex = 39999999
+Version.Parent = ScreenGui
+
+task.spawn(function()
+local TweenService = game:GetService("TweenService")
+local lp = game.Players.LocalPlayer
+local char = lp.Character or lp.CharacterAdded:Wait()
+local hrp = char:WaitForChild("HumanoidRootPart")
+
+local targetCFrame = CFrame.new(Vector3.new(3.372170925140381, 6.251251220703125, 38088.98046875))
+local speed = 150
+
+local distance = (hrp.Position - targetCFrame.Position).Magnitude
+local time = distance / speed
+
+local tweenInfo = TweenInfo.new(time, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
+local tween = TweenService:Create(hrp, tweenInfo, {CFrame = targetCFrame})
+tween:Play()
+    end)
+  end
+})
 --{ Lưu Conifg }--
 SaveManager:SetLibrary(Library)
 InterfaceManager:SetLibrary(Library)
